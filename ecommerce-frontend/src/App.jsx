9479d41c444770d24c1ac6fa8421abb3a25ba92b
YakupEmreCelebi <ykpmrclb06@gmail.com>
2025-11-18 16:41:34 +0300

    Implemented Footer.jsx component

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
 
-import Header from './components/Header'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   );
 }
---
+++ ecommerce-frontend/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons"; 
+
+import styles from './css/Footer.module.css';
+
+function Footer() {
+
+    return(
+
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                <span>Built with</span>
+                <FontAwesomeIcon icon={faHeart} className={styles.icon}/>
+                <a href="">Celebi</a>
+            </div>
+        </footer>
+        
+    );
+}
+export default Footer
