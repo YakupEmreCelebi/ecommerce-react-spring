16e804a4160c83a55a2d5276612ff0d954d0640e
YakupEmreCelebi <ykpmrclb06@gmail.com>
2026-01-08 16:34:51 +0300

    chore: add mysql driver starter

--- ecommerce-backend/src/main/java/com/example/ecommerce_backend/EcommerceBackendApplication.java
+++ ecommerce-backend/src/main/java/com/example/ecommerce_backend/EcommerceBackendApplication.java
@@ -2,9 +2,13 @@ package com.example.ecommerce_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EntityScan
 public class EcommerceBackendApplication {
 
 	public static void main(String[] args) {
