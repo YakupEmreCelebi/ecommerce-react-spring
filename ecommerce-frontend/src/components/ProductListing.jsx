6a9006e826396c70a882d655af9a8ba567c12512
YakupEmreCelebi <ykpmrclb06@gmail.com>
2026-01-08 15:06:48 +0300

    feat: add SearchBox and CategoryDropdown components to HomePage

--- ecommerce-frontend/src/components/ProductListing.jsx
+++ ecommerce-frontend/src/components/ProductListing.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
+import React from 'react';
 
 import styles from './css/ProductListing.module.css';
-
 import ProductCard from './ProductCard';
+import SearchBox from './SearchBox';
+import Dropdown from './Dropdown';
 
-function ProductListing({products}) {
 
-
-  if(products.length === 0)
-  {
-    return(
-      <div style={{height:"70vh", display:"flex", alignItems:"center"}}>
-        <h1 style={{fontFamily: "var(--font-family)"}}>No products found</h1>
-      </div>
-    );
-  }
-    
+function ProductListing({ products }) {
   return (
-    <div className={styles.container}>
-        {products.map((p, index) => <ProductCard product={p} key={index}/>)}
+
+    <div className='flex flex-col'>
+      <div className='flex flex-col sm:flex-row justify-between items-center gap-4 pt-12'>
+        <SearchBox label={"Search"} placeholder={"Search Products..."} value=""></SearchBox>
+        <Dropdown label="Sort by" options={["Popularity", "Price Low to High", "Price High to Low"]} value="Popularity"></Dropdown>
+      </div>
+
+      <div className={styles.container}>
+      
+        {products.length === 0 ? (
+          <div
+            style={{
+              height: "70vh",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
+            }}
+          >
+            <h1 style={{ fontFamily: "var(--font-family)" }}>
+              No products found
+            </h1>
+          </div>
+        ) : (
+          products.map((p, index) => (
+            <ProductCard product={p} key={index} />
+          ))
+        )}
+      </div>
     </div>
-  )
+    
+  );
 }
 
-export default ProductListing
+export default ProductListing;
