fbc298f7bf148273910446b15d98d09913e3c0e6
YakupEmreCelebi <ykpmrclb06@gmail.com>
2026-01-12 17:07:43 +0300

    feat: implement About Page UI

--- ecommerce-frontend/src/pages/About.jsx
+++ ecommerce-frontend/src/pages/About.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import PageHeading from '../components/PageHeading';
 
 function About() {
   return (
-    <div>About</div>
+    <div className='flex flex-col min-w-[100vh]'>
+      <div className='flex flex-col items-center'>
+        <PageHeading title={"About Us"}>
+          This project is a personal demo application developed for learning and portfolio purposes.
+          <br />
+          <br />
+          It was created to practice modern web development concepts such as frontend–backend integration,
+          RESTful APIs, component-based UI design, and responsive layouts. The main focus is on writing clean code,
+          building scalable architecture, and understanding real-world development workflows.
+          <br />
+          <br />
+          While this is not a production or commercial product, 
+          it represents my approach to software engineering, problem solving, and continuous learning.
+        </PageHeading>
+      </div>
+    </div>
   )
 }
 
